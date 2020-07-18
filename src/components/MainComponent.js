28e64e409fc4aec59c9d4d768e1917f2fead06a8
Yasmine Eldemery <yasmineldemery@yahoo.com>
2020-07-18 22:33:29 +0200

    Assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,8 @@ import React, { Component} from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from "./ContactComponent";
+import About from "./AboutComponent";
+import DishDetail from "./DishdetailComponent";
 import Header from "./HeaderComponent";
 import Footer from './FooterComponent';
 import {DISHES} from '../shared/dishes';
@@ -34,13 +36,21 @@ class Main extends Component {
             leader={this.state.leaders.filter((leader) => leader.featured)[0]}/>
         )
     }
+    const DishWithId = ({match}) => {
+        return(
+            <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+              comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+        );
+      };
     return (
         <div>
         <Header />
         <Switch>
             <Route path="/home" component={HomePage} />
             <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>}/>
+            <Route path='/menu/:dishId' component={DishWithId} />
             <Route exact path="/contactus" component={Contact} />
+            <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders}/>} />
             <Redirect to="/home" />
         </Switch>
         <Footer />
